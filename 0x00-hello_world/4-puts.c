0bb2a116ea403c531a83f90fa3b028c04451d59d
Natan-Mesele <huluale12@gmail.com>
2022-07-02 10:49:30 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,4 @@
 #include <stdio.h>
-/**
- *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
