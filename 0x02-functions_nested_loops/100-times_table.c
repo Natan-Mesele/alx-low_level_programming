c168f5f81449cc979b22b7b18e7a62ec24886c16
Natan-Mesele <huluale12@gmail.com>
2022-07-06 13:05:59 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,7 @@ _putchar(k + '0');
 else if (k < 10 && j != 0)
 {
 _putchar(',');
-_putchar(' ');
+_putchar(' '); 
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
@@ -29,9 +29,9 @@ else if (k >= 10 && k < 100)
 {
 _putchar(',');
 _putchar(' ');
-_putchar(' ');										           
+_putchar(' ');
 _putchar((k / 10) + '0');
-_putchar((k % 10) + '0');										    
+_putchar((k % 10) + '0');
 }
 else if (k >= 100)
 {
@@ -39,7 +39,7 @@ _putchar(',');
 _putchar(' ');
 _putchar((k / 100) + '0');
 _putchar(((k / 10) % 10) + '0');
-_putchar((k % 10) + '0);				
+_putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
