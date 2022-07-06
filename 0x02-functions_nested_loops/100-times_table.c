a1d495f1e6af39f82fadf6ef8a106c1361fe5fed
Natan-Mesele <huluale12@gmail.com>
2022-07-06 09:37:49 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *  print_times_table - Entry point
- *  @n: input
- *  Return: Always 0 (Success)
+ * print_times_table - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
  */
 void print_times_table(int n)
 {
