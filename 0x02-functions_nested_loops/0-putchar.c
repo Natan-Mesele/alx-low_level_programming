bfe85cef2e136de700b4cff4b3bba965b61643ad
Natan-Mesele <huluale12@gmail.com>
2022-07-06 05:59:36 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,12 @@
  */
 int main(void)
 {
-_putchar(' '); 
-_putchar('p'); 
-_putchar('u'); 
-_putchar('t'); 
-_putchar('c'); 
-_putchar('h'); 
-_putchar('a');
-_putchar('r'); 
+char *sh = "Holberton";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
 _putchar('\n');
 return (0);
 }
