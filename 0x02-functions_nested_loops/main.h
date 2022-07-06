fcbedbaaff07bf059cb3ecc0c0afa10ccc9ec9ad
Natan-Mesele <huluale12@gmail.com>
2022-07-06 05:49:00 -0700

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_number(void);
+void print_most_number(void);
+void more_number(void);
+void print_line(int n);
+void print_ddiagonal(int n);
+void print_square(int size);
