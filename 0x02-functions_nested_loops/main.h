6322e00c3f6e9a54fb7ded38552b785ddf823cb2
Natan-Mesele <huluale12@gmail.com>
2022-07-06 05:55:40 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,16 @@
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_number(void);
-void print_most_number(void);
-void more_number(void);
-void print_line(int n);
-void print_ddiagonal(int n);
-void print_square(int size);
+#ifndef MAIN_H
+#define MAIN_H
+char _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int nld);
+void jack_bauer(void);
+void times_table(void);
+int add(int one, int two);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif /* MAIN_H */
