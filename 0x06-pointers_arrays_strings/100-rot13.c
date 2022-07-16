a747ba93c43cb007c0adaa9857bb1e30c6d69aae
Natan-Mesele <huluale12@gmail.com>
2022-07-16 11:19:36 -0700

    rot 13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,42 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * rot13 - Encodes a cing using rot13.
- * @c: The cing to be encoded.
- * Return: A pointer to the encoded cing.
+ * rot13 - encoding to rot 13
+ * @s: the string to be coded
+ * Return: address of s
  */
-char *rot13(char *c)
+char *rot13(char *s)
 {
-int i = 0, j;
-char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-'G', 'H', 'I', 'J', 'K', 'L',
-'M', 'N', 'O', 'P', 'Q', 'R',
-'S', 'T', 'U', 'V', 'W', 'X',
-'Y', 'Z', 'a', 'b', 'c', 'd',
-'e', 'f', 'g', 'h', 'i', 'j',
-'k', 'l', 'm', 'n', 'o', 'p',
-'q', 'r', 's', 't', 'u', 'v',
-'w', 'x', 'y', 'z'};
-char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-'T', 'U', 'V', 'W', 'X', 'Y',
-'Z', 'A', 'B', 'C', 'D', 'E',
-'F', 'G', 'H', 'I', 'J', 'K',
-'L', 'M', 'n', 'o', 'p', 'q',
-'r', 's', 't', 'u', 'v', 'w',
-'x', 'y', 'z', 'a', 'b', 'c',
-'d', 'e', 'f', 'g', 'h', 'i',
-'j', 'k', 'l', 'm'};
-while (c[i])
+int i, j;
+char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHILJLMNOPQRSTUVWXYZ";
+char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+for (i = 0; *(s + i); i++)
 {
 for (j = 0; j < 52; j++)
 {
-if (c[i] == alphabet[j]
+if (a[j] == *(s + i))
 {
-c[i] = rot13key[j];
+*(s + i) = b[j];
 break;
 }
 }
-i++;
 }
-return (c);
+return (s);
 }
