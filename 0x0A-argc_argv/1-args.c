de7d4b84564d89315792c83f308047f28c2b360a
Natan-Mesele <huluale12@gmail.com>
2022-07-23 14:54:29 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,13 +7,8 @@
  */
 int main(int argc, char *argv[])
 {
-int i;
-if (argc >= 1)
-{
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
-}
+/* to suppress unused variable warning */
+argv = argv;
+printf("%d\n", argc - 1);
 return (0);
 }
